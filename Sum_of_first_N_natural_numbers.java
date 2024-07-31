6888f88892943f802442d6b181d4f1227b15c3e1
sairamvenkatatulasi <147791941+sairamvenkatatulasi@users.noreply.github.com>
2024-07-31 15:48:23 +0530

    import java.util.Scanner; public class  example{     public static void main(String[] args){      int sum,n;       Scanner read = new Scanner(System.in);       n=read.nextInt();       sum=n*(n+1)/2;       System.out.println(sum);
    }
    }

---
+++ Sum_of_first_N_natural_numbers.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class  example{
+    public static void main(String[] args){
+     int sum,n;
+      Scanner read = new Scanner(System.in);
+      n=read.nextInt();
+      sum=n*(n+1)/2;
+      System.out.println(sum);
+    
+}
+}
